de8e11c48a5e241ca8bc9accfe992db8860d61c5
22f2001193 <22f2001193@ds.study.iitm.ac.in>
2026-03-21 21:53:40 +0530

    version 1.0 apk build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,15 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../run-release-key.jks")
+            storePassword = "runapp123"
+            keyAlias = "run-key"
+            keyPassword = "runapp123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -29,6 +38,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
